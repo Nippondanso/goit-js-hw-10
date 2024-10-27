d23ef2db10518572b2136051a5d4d39548e7d0b6
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-10-27 20:14:29 +0200

    h10 update:
    second task done.

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,23 +1,43 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 
 const form = document.querySelector('.form');
 const delayInput = document.querySelector('.delay-input');
 
 
+const createPromise = (delay, state) => {
+  if (state === 'fulfilled') {
+    setTimeout(() => {
+      console.log(`✅ Fulfilled promise in ${delay}ms`);
+      iziToast.success(
+        {
+          message: `Fulfilled promise in ${delay} ms`,
+        },
+      );
+    }, delay);
+  } else {
+    setTimeout(() => {
+      console.log(`❌ Rejected promise in ${delay}ms`);
+      iziToast.error(
+        {
+          message: `Rejected promise in ${delay} ms`,
+        },
+      );
+    }, delay);
+  }
+};
+
 form.addEventListener(
-  "submit",
-  function(event)  {
+  'submit',
+  function(event) {
     event.preventDefault();
 
     const formData = new FormData(this);
-
     const delayValue = formData.get('delay');
     const stateValue = formData.get('state');
 
-    console.log('Delay (ms):', delayValue);
-    console.log('State:', stateValue);
+    createPromise(delayValue, stateValue);
   },
   false,
 );
